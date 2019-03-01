fa6f9a481b30d502294cc8bfd2ecae58ecb4169c
kazeem taiwo adeyinka <alevel7@gmail.com>
2019-03-01 02:09:44 +0100

    add dropdown script

---
+++ ui/js/script.js
@@ -0,0 +1,13 @@
+//codes for the mobile platform
+let toggle = 0;
+let menuButton=document.getElementsByClassName("dropdown");
+menuButton[0].addEventListener("click", ()=>{
+    if(toggle==0){
+        document.getElementById("dropmenu").style.display = "block";
+        toggle++;
+    }else{
+        document.getElementById("dropmenu").style.display = "none";
+        toggle--;
+    }
+   
+})
