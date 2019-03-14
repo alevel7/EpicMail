f1e3e9d9a4ef6989634c59b9c971d6cf5151c245
kazeem taiwo adeyinka <alevel7@gmail.com>
2019-03-14 11:43:56 +0100

    [fixes #164534434] update mock database

--- db/models/User.js
+++ db/models/User.js
@@ -1,62 +1,40 @@
 const uid = require('rand-token').uid;
-
-
-module.exports = {
-    currentUser: class User {
-        constructor() {
-        }
-        create(data) {
-            const newUser = {
-                'token': uid(16),
-                'email': data.email || '',
-                'firstName': data.firstName || '',
-                'lastName': data.lastName || '',
-                'password': data.password || '',
-                'login': false
-            };
-            return newUser;
-        }
-
-    },
-    db: class user_db {
-        constructor() {
-            this.users = [ {
-                "token": "S64N1bNiEK6994ua",
-                "email": "kazeem@epicmail.com",
-                "firstName": "kazem",
-                "lastName": "me",
-                "password": "sgff4356",
-                "login": false,
-                "id": 1
-              },
-              {
-                "token": "Cq8CN0w9de42ge0i",
-                "email": "alevel7@gmail.com",
-                "firstName": "kazem",
-                "lastName": "me",
-                "password": "sgff4356",
-                "login": false,
-                "id": 2
-              }]
-            this.id = this.users.length+1;
-        }
-        save(user) {
-            this.users.push(user);
-            this.id++;
-        }
-        findOne(email) {
-            return this.users.find(user => user.email === email);
-        }
-        getId(email) {
-            return this.findOne(email);
-        }
-        isLoggedIn(email) {
-            return this.findOne(email).login;
-        }
-        logIn(email) {
-            let index = this.users.indexOf(this.findOne(email));
-            this.users[index].login = true;
-            return true;
-        }
+module.exports={
+    user:class User{
+    constructor(){
+        this.db = [{
+            "email": "alevel7@epicmail.com",
+            "firstName": "kazem",
+            "lastName": "me",
+            "password": "12345",
+            "id":0    
+        }]
+        this.id = this.db.length
     }
+    create(data){
+        const newUser={
+            token:uid(16),
+            email:data.email,
+            firstName:data.firstName,
+            lastName:data.lastName,
+            password:data.password,
+        }
+        newUser.id = this.id
+        this.id+=1;
+        this.db.push(newUser)
+        return newUser;
+    }
+    findOne(email){
+        return this.db.find(user=>user.email == email)
+    }
+    findAll(){
+        return this.db
+    }
+    deleteOne(email){
+        let index = this.db.findIndex(email=>email.email === email)
+        this.db.splice(index, 1)
+        return {}
+    }
+
+}
 }
--- db/models/message.js
+++ db/models/message.js
@@ -1,72 +1,41 @@
-const uid = require('rand-token').uid;
+
 module.exports = {
-        Message: class Message {
-            constructor(sender) {
-                this.messageId = 0;
-                this.parentMessageId = 0;
-            }
-            create(data) {
-                const newMessage = {
-                    'senderId': data.senderId,
-                    'recieverId': data.recieverId,
-                    'createdOn': new Date(),
-                    'subject': data.subject,
-                    'message': data.message,
-                    'status': data.status
-                };
-                return newMessage;
-            }
-        },
-        db: class message_db {
-            constructor() {
-                this.messages = [{
-                    "senderId": 1,
-                    "recieverId": 2,
-                    "createdOn": "2019-03-08T21:39:12.330Z",
-                    "subject": "entry cup championship",
-                    "message": "win to win the championship",
-                    "status": "unread",
-                    "id": 1
-                  },{
-                    "senderId": 1,
-                    "recieverId": 2,
-                    "createdOn": "2019-03-08T21:39:12.330Z",
-                    "subject": "entry cup championship",
-                    "message": "win to win the championship",
-                    "status": "sent",
-                    "id": 2
-                  }];
-                this.id = 1;
-            }
-            save(message) {
-                this.messages.push(message);
-                this.id++;
-            }
-            findOne(id) {
-                return this.messages.find(message => message.id === id);
-            }
-            allSent(status) {
-                return this.messages.filter((message) => {
-                    return message.status === status;
-                })
-            }
-            
-            allUnread(status) {
-                return this.messages.filter((message) => {
-                    return message.status === status;
-                })
-            }
-            allDraft(status) {
-                return this.messages.filter((message) => {
-                    return message.status === 'draft';
-                })
-            }
-            allReceived(id) {
-                let theMessages = this.messages.filter(message => message.recieverId === id);
-        
-                return theMessages.filter((message) => {
-                    return message.status === "read" || message.status === "unread";
-                })
-            }
+    Message: class Message {
+        constructor() {
+            this.db = []
+            this.id = 0
+        }
+        create(data) {
+            const newMessage = {
+                'createdOn': new Date().toDateString(),
+                'subject': data.subject,
+                'message': data.message,
+            };
+            return newMessage;
+        }
+        save(data){
+            data.id = this.id
+            data.parentMessageId = this.id;
+            this.id+=1;
+            this.db.push(data)
+            return data
+        }
+
+        findOne(id) {
+            return this.db.find(message => message.id === id);
+        }
+        allMessages(){
+            return this.db
+        }
+        allOfSent(status,id){
+            return this.db.filter(message=>{
+                return message.senderStatus == status && message.senderId === id
+            })
+        }
+        allOfRecieved(status,id){
+            return this.db.filter(message=>{
+                return message.recieverStatus == status && message.recieverId === id
+            })
         }
     }
+}
